61592be95ec405764d73ca89297014cc647c82b5
CloudSkillGuru <cloudskillsboostacademy@gmail.com>
2025-01-15 09:10:05 +0530

    2nd commit

--- lab_6_setup.sh
+++ lab_6_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Lab Setup Script
-wget https://raw.githubusercontent.com/ACloudGuru/advanced-terraform-with-azure/main/lab_vm_scale_set_resources/providers.tf
-wget https://raw.githubusercontent.com/ACloudGuru/advanced-terraform-with-azure/main/lab_vm_scale_set_resources/networking.tf
+wget https://raw.githubusercontent.com/CloudSkillGuru/advanced_terraform_with-azure/refs/heads/main/lab_vm_scale_set_resources/networking.tf
+wget https://raw.githubusercontent.com/CloudSkillGuru/advanced_terraform_with-azure/refs/heads/main/lab_vm_scale_set_resources/providers.tf
 mkdir terraformguru
 mv providers.tf terraformguru/
 mv networking.tf terraformguru/
